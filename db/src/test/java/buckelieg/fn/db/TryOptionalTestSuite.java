9f9768d508d54ad2b20f97d8dee417b66619fa66
git <git@github.com>
2017-12-07 22:34:48 +0300

    tests upd.

--- db/src/test/java/buckelieg/fn/db/TryOptionalTestSuite.java
+++ db/src/test/java/buckelieg/fn/db/TryOptionalTestSuite.java
@@ -9,12 +9,12 @@ import static org.junit.Assert.assertTrue;
 public class TryOptionalTestSuite {
 
     @Test
-    public void testMap() throws Exception {
+    public void testMap() {
         assertTrue(10L == TryOptional.of(() -> 5L).map(x -> x * 2).toOptional().orElse(0L));
     }
 
     @Test(expected = SQLException.class)
-    public void testException() throws Throwable {
+    public void testException() {
         TryOptional.of(() -> {
             throw new SQLException("TEST");
         }).map(x -> 5).map(x -> {
@@ -23,11 +23,18 @@ public class TryOptionalTestSuite {
     }
 
     @Test
-    public void testRecover() throws Throwable {
+    public void testRecover() {
         assertTrue(
                 "1".equals(TryOptional.of(() -> 5).map(x -> {
                     throw new UnsupportedOperationException("" + x);
                 }).recover(e -> "1").get())
         );
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testGetOptional() {
+        TryOptional.of(() -> {
+            throw new NullPointerException();
+        }).getOptional();
     }
 }
