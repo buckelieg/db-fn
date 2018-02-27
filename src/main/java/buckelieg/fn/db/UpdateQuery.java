b0ada8fe18344f51eb6b68e401f03381c68490e0
Anatoly <buckelieg@gmail.com>
2018-02-27 12:56:25 +0300

    Add files via upload

--- src/main/java/buckelieg/fn/db/UpdateQuery.java
+++ src/main/java/buckelieg/fn/db/UpdateQuery.java
@@ -1,18 +1,18 @@
 /*
-* Copyright 2016-2017 Anatoly Kutyakov
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*    http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright 2016-2017 Anatoly Kutyakov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package buckelieg.fn.db;
 
 import javax.annotation.Nonnull;
@@ -75,9 +75,8 @@ final class UpdateQuery extends AbstractQuery<TryOptional<Long>, PreparedStateme
     @Nonnull
     @Override
     public TryOptional<Long> execute() {
-        return TryOptional.of(() -> {
-            try {
-                return jdbcTry(() -> {
+        return TryOptional.of(() ->
+                jdbcTry(() -> {
                     Connection conn = connectionSupplier.get();
                     boolean autoCommit = true;
                     Savepoint savepoint = null;
@@ -114,11 +113,7 @@ final class UpdateQuery extends AbstractQuery<TryOptional<Long>, PreparedStateme
                         }
                         close();
                     }
-                });
-            } catch (Throwable t) {
-                throw new SQLException(t);
-            }
-        });
+                }));
     }
 
     private long executeSimple() {
